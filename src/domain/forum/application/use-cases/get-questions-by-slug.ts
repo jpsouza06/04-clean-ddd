b297faab0e9f1720d2167e396b824c279b4a8557
Joao Paulo <joao.souza@atsinformatica.com.br>
2023-09-19 14:41:28 -0300

    add use cases

---
+++ src/domain/forum/application/use-cases/get-questions-by-slug.ts
@@ -0,0 +1,32 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id';
+import { Question } from '../../enterprise/entities/question';
+import { QuestionRepository } from '../repositories/question-repository';
+
+interface GetQuestionBySlugUseCaseRequest {
+   slug: string
+}
+
+interface GetQuestionBySlugUseCaseResponse {
+   question: Question
+}
+
+export class GetQuestionBySlugUseCase {
+   constructor(
+      private questionRepository: QuestionRepository,
+   ) {}
+   async execute(
+      {
+         slug
+      }: GetQuestionBySlugUseCaseRequest): Promise<GetQuestionBySlugUseCaseResponse> {
+         console.log(slug)
+         const question = await this.questionRepository.findBySlug(slug)
+
+         if (!question) {
+            throw new Error('Question not found')
+         }
+
+         return {
+            question,
+         }
+   }
+}
