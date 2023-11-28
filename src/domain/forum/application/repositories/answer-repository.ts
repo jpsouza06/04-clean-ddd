3beb0b126da0f566d42e0d56705b198efec3529b
João Paulo <joaopaulosc06@gmail.com>
2023-11-27 21:42:32 -0300

    add use cases

--- src/domain/forum/application/repositories/answer-repository.ts
+++ src/domain/forum/application/repositories/answer-repository.ts
@@ -1,7 +1,9 @@
+import { PaginationParams } from "@/core/repositories/paginations-params";
 import { Answer } from "../../enterprise/entities/answer";
 
 export interface AnswersRepository {
    findById(answerId: string): Promise<Answer | null>
+   findManyByQuestionId(questionId: string, { page }: PaginationParams): Promise<Answer[]>
    save(question: Answer): Promise<void>
    create(answer: Answer): Promise<void>
    delete(answer: Answer): Promise<void>
--- src/domain/forum/application/use-cases/get-questions-by-slug.ts
+++ src/domain/forum/application/use-cases/get-questions-by-slug.ts
@@ -1,4 +1,3 @@
-import { UniqueEntityId } from '@/core/entities/unique-entity-id';
 import { Question } from '../../enterprise/entities/question';
 import { QuestionRepository } from '../repositories/question-repository';
 
