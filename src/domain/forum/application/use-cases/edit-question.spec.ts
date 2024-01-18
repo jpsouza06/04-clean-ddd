e85012350bebc85d0aebbb39b32fd299fa3a6f12
João Paulo <joaopaulosc06@gmail.com>
2024-01-17 21:54:40 -0300

    add WatchedList

--- src/domain/forum/application/use-cases/edit-question.spec.ts
+++ src/domain/forum/application/use-cases/edit-question.spec.ts
@@ -3,13 +3,17 @@ import { makeQuestion } from 'test/factories/make-question';
 import { EditQuestionUseCase } from './edit-question';
 import { UniqueEntityId } from '@/core/entities/unique-entity-id';
 import { NotAllowedError } from './errors/not-allowed-error';
+import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/in-memory-question-attachemnt-repository';
+import { makeQuestionAttachment } from 'test/factories/make-question-attachment';
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryQuestionAttachmentRepository: InMemoryQuestionAttachmentsRepository
 let sut: EditQuestionUseCase
 describe('Edit Question', () => {
    beforeEach(() => {
       inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
-      sut = new EditQuestionUseCase(inMemoryQuestionsRepository)
+      inMemoryQuestionAttachmentRepository = new InMemoryQuestionAttachmentsRepository()
+      sut = new EditQuestionUseCase(inMemoryQuestionsRepository, inMemoryQuestionAttachmentRepository)
    })
 
    it('should be able to edit a question', async () => {
@@ -19,17 +23,34 @@ describe('Edit Question', () => {
 
       await inMemoryQuestionsRepository.create(newQuestion)
 
+      await inMemoryQuestionAttachmentRepository.items.push(
+         makeQuestionAttachment({
+            questionId: newQuestion.id,
+            attachmentId: new UniqueEntityId('1')
+         }),
+         makeQuestionAttachment({
+            questionId: newQuestion.id,
+            attachmentId: new UniqueEntityId('2')
+         })
+      )
+
       await sut.execute({
          questionId: newQuestion.id.toString(),
          authorId: 'author-1',
          title: 'Titulo teste',
-         content: 'Conteúdo teste'
+         content: 'Conteúdo teste',
+         attachmentsIds: ['1', '3'],
       })
 
       expect(inMemoryQuestionsRepository.items[0]).toMatchObject({
          title: 'Titulo teste',
          content: 'Conteúdo teste'
       })
+      expect(inMemoryQuestionsRepository.items[0].attachments.currentItems).toHaveLength(2)
+      expect(inMemoryQuestionsRepository.items[0].attachments.currentItems).toEqual([
+         expect.objectContaining({ attachmentId: new UniqueEntityId('1') }),
+         expect.objectContaining({ attachmentId: new UniqueEntityId('3') })
+      ])
    })
 
    it('should not be able to edit a question from another user', async () => {
@@ -43,7 +64,8 @@ describe('Edit Question', () => {
          questionId: newQuestion.id.toValue(),
          authorId: 'author-2',
          title: 'Titulo teste',
-         content: 'Conteúdo teste'
+         content: 'Conteúdo teste',
+         attachmentsIds: [],
       })
 
       expect(result.isLeft()).toBe(true)
--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -4,6 +4,7 @@ import { Optional } from "@/core/types/optional"
 import { Slug } from "./value-objects/slug"
 import { AggregateRoot } from "@/core/entities/aggregate-root"
 import { QuestionAttachment } from "./question-attachment"
+import { QuestionAttachmentList } from "./question-attachment-list"
 
 export interface QuestionProps {
    authorId: UniqueEntityId
@@ -11,7 +12,7 @@ export interface QuestionProps {
    title: string
    content: string
    slug: Slug
-   attachments: QuestionAttachment[]
+   attachments: QuestionAttachmentList
    createdAt: Date
    updatedAt?: Date
 }
@@ -72,7 +73,7 @@ export class Question extends AggregateRoot<QuestionProps>  {
       this.touch()
    }
 
-   set attachments(attachments: QuestionAttachment[]) {
+   set attachments(attachments: QuestionAttachmentList) {
       this.props.attachments = attachments
    }
 
@@ -88,7 +89,7 @@ export class Question extends AggregateRoot<QuestionProps>  {
       const question = new Question({
          ...props,
          slug: props.slug ?? Slug.createFromText(props.title),
-         attachments: props.attachments ?? [],
+         attachments: props.attachments ?? new QuestionAttachmentList(),
          createdAt: props.createdAt ?? new Date(),
       }, id)
 
